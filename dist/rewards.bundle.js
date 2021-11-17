38e7fd2a9cf39990b4861352401dea39af1c4c66
appletri <tri_duc_lam@hotmail.com>
2021-11-16 18:11:54 -0700

    started rewards page

--- dist/rewards.bundle.js
+++ dist/rewards.bundle.js
@@ -10,20 +10,51 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/function.js":
+/*!*************************!*\
+  !*** ./src/function.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"addText\": () => (/* binding */ addText),\n/* harmony export */   \"addContainer\": () => (/* binding */ addContainer),\n/* harmony export */   \"addIconLabel\": () => (/* binding */ addIconLabel)\n/* harmony export */ });\n\n\nfunction addText(num, cssName, parent ) {\n    for (let i=1; i<=num; i++){\n        const element = document.createElement('p');\n        element.className = cssName + i;\n        parent.appendChild(element);\n    }\n    \n};\n\nfunction addContainer(num, cssName, parent) {\n    for (let i=1; i<=num; i++){\n        const element = document.createElement('div');\n        element.className = cssName + i;\n        parent.appendChild(element);\n    }\n\n};\n\nfunction addIconLabel(parent, arrText, arrPic){\n    for (let i=0; i<arrText.length; i++){        \n        const element = document.createElement('div');\n        function addDivForImg (){\n            const imgMask = document.createElement('div');\n            const drink = new Image();\n            drink.src = arrPic[i];\n            imgMask.className = 'imgMask'\n            imgMask.appendChild(drink);\n            element.appendChild(imgMask);\n        }\n        addDivForImg();\n        const text = document.createElement('p');\n        text.textContent = arrText[i];\n        element.appendChild(text);\n        element.className = `menuIcon`;\n        parent.appendChild(element);\n    }\n};\n\n//# sourceURL=webpack://restraurant-page/./src/function.js?");
+
+/***/ }),
+
 /***/ "./src/pages/rewards.js":
 /*!******************************!*\
   !*** ./src/pages/rewards.js ***!
   \******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ startUpRewards)\n/* harmony export */ });\nconsole.log('rewards page');\n\nfunction startUpRewards(){\n    // document.body.appendChild(content());\n    const main = document.querySelector('#content');\n    main.textContent = 'This is the Rewards page, check it out'\n   \n   \n   \n    function addDiv() {\n        const element = document.createElement('div');\n        element.id = 'content';\n        return element;\n    };\n}\n\n//# sourceURL=webpack://restraurant-page/./src/pages/rewards.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ startUpRewards)\n/* harmony export */ });\n/* harmony import */ var _function_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../function.js */ \"./src/function.js\");\n\n\nfunction startUpRewards(){\n    (0,_function_js__WEBPACK_IMPORTED_MODULE_0__.addContainer)(1,'mainRewards',document.querySelector('#content'))\n    ;(0,_function_js__WEBPACK_IMPORTED_MODULE_0__.addContainer)(4,'rewards',document.querySelector('.mainRewards1'));\n    const rewards1 = document.querySelector('.rewards1');\n    (0,_function_js__WEBPACK_IMPORTED_MODULE_0__.addContainer)(1,'box',rewards1);\n    (0,_function_js__WEBPACK_IMPORTED_MODULE_0__.addText)(3,'R1text',document.querySelector('.box1'));\n    document.querySelector('.R1text1').textContent = 'FREE COFFEE IS A TAP AWAY';\n    document.querySelector('.R1text2').textContent = 'Join now to start earning Rewards';\n    document.querySelector('.R1text3').textContent = 'Or join in the app for the best experience';\n    const hero = new Image();\n    hero.src = 'https://www.starbucks.com/weblx/images/rewards/hero/xl-hero-desktop_2021.png';\n    rewards1.appendChild(hero);\n}\n\n//# sourceURL=webpack://restraurant-page/./src/pages/rewards.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
@@ -59,8 +90,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/pages/rewards.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/pages/rewards.js");
 /******/ 	
 /******/ })()
 ;
--- src/pages/rewards.js
+++ src/pages/rewards.js
@@ -1,15 +1,15 @@
-console.log('rewards page');
+import {addText , addContainer} from '../function.js';
 
 export default function startUpRewards(){
-    // document.body.appendChild(content());
-    const main = document.querySelector('#content');
-    main.textContent = 'This is the Rewards page, check it out'
-   
-   
-   
-    function addDiv() {
-        const element = document.createElement('div');
-        element.id = 'content';
-        return element;
-    };
+    addContainer(1,'mainRewards',document.querySelector('#content'))
+    addContainer(4,'rewards',document.querySelector('.mainRewards1'));
+    const rewards1 = document.querySelector('.rewards1');
+    addContainer(1,'box',rewards1);
+    addText(3,'R1text',document.querySelector('.box1'));
+    document.querySelector('.R1text1').textContent = 'FREE COFFEE IS A TAP AWAY';
+    document.querySelector('.R1text2').textContent = 'Join now to start earning Rewards';
+    document.querySelector('.R1text3').textContent = 'Or join in the app for the best experience';
+    const hero = new Image();
+    hero.src = 'https://www.starbucks.com/weblx/images/rewards/hero/xl-hero-desktop_2021.png';
+    rewards1.appendChild(hero);
 }
